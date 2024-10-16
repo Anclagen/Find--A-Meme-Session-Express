2fa1292fbdf2b97939a6c087d94fe26a6915ca10
Anclagen <91462616+Anclagen@users.noreply.github.com>
2024-10-16 03:06:25 +0200

    added login passport code

--- routes/login.js
+++ routes/login.js
@@ -1,4 +1,9 @@
 var express = require("express");
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
+const path = require("path");
+const fs = require("fs");
+
 var router = express.Router();
 
 /* GET home page. */
@@ -6,12 +11,48 @@ router.get("/", function (req, res, next) {
   res.render("login");
 });
 
-router.post("/", function (req, res, next) {
-  res.render("login");
+passport.use(
+  new LocalStrategy(function verify(username, password, cb) {
+    let usersArray = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../data/users.json")));
+    let filteredArray = usersArray.filter((x) => x.username == username);
+    if (filteredArray.length > 0) {
+      let usersData = filteredArray[0];
+      if (usersData.password == password) {
+        return cb(null, usersData);
+      }
+    } else {
+      return cb(null, false);
+    }
+  })
+);
+
+passport.serializeUser((user, callback) => {
+  callback(null, user);
 });
 
-router.post("/logout", function (req, res, next) {
-  res.render("login");
+passport.deserializeUser((user, callback) => {
+  const userId = user ? user.username : "";
+  callback(null, userId);
+});
+
+router.post(
+  "/password",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  }),
+  (req, res) => {
+    res.redirect("/memes");
+  }
+);
+
+router.get("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/memes");
+  });
 });
 
 module.exports = router;
